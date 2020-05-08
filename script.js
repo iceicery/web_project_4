e7e3c8f16c0396c693a35d227e47a1424762b034
Pinwei <pw.amy.wu@gmail.com>
2020-05-08 09:25:48 -0500

    feat:js for calling form

---
+++ script.js
@@ -0,0 +1,46 @@
+//call and dismiss the form//
+
+let container=document.querySelector('.container');
+let editButton=container.querySelector('.profile__button-sqr');
+let editForm=container.querySelector('.edit');
+let cancelButton=container.querySelector('.edit__icon');
+
+
+function displayEdit(){
+    editForm.classList.remove('edit__hidden');
+}
+function hiddenEdit(){
+    editForm.classList.add('edit__hidden');
+}
+
+
+editButton.addEventListener("click",displayEdit);
+cancelButton.addEventListener("click",hiddenEdit);
+
+
+
+// Let's find the form in the DOM
+let formElement = container.querySelector('.edit__button');
+
+// Next is the form submit handler, though
+// it won't submit anywhere just yet
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // This line stops the browser from submitting the form in the default way.
+                                                // Having done so, we can define our own way of submitting the form.
+                                                // We'll explain it in more detail later.
+
+    // Let's find the form fields in the DOM
+    let nameInput = container.querySelector('#name');
+    let jobInput = container.querySelector('#job');// Use querySelector()
+    console.log(nameInput.value);
+    // Get the values of each field from the corresponding value property
+
+ 
+    // Select elements where the field values will be entered
+
+    // Insert new values using the textContent property of the querySelector() method
+}
+
+// Connect the handler to the form:
+// it will watch the submit event
+formElement.addEventListener('submit', formSubmitHandler);
