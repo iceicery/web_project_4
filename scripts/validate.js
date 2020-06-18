e254b0e4ff1a17540d57a0c5b9c395f00716ba43
Pinwei <pw.amy.wu@gmail.com>
2020-06-18 15:23:46 -0500

    create class FormValidator

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,89 +1,100 @@
-const showInputError = (formSelector, inputElement, errorMessage) => {
-    const errorElement = formSelector.querySelector(`#${inputElement.id}-error`);
-    inputElement.classList.add("formInput-error");
-    errorElement.textContent = errorMessage;
-    errorElement.classList.add("formInput-errorMessage");
-};
 
-
-const hideInputError = (formSelector, inputElement) => {
-    const errorElement = formSelector.querySelector(`#${inputElement.id}-error`);
-    inputElement.classList.remove("formInput-error");
-    errorElement.classList.remove("formInput-errorMessage");
-    errorElement.textContent = "";
-};
-
-const checkInputValidity = (formSelector, inputElement) => {
-    if (!inputElement.validity.valid) {
-        showInputError(formSelector, inputElement, inputElement.validationMessage);
+class FormValidator{
+    constructor(object,formSelector){
+        this._object=object;
+        this._formSelector=formSelector;
     }
-    else {
-        hideInputError(formSelector, inputElement);
+    _showInputError(inputElement,errorMessage){
+        const errorElement = this._formSelector.querySelector(`#${inputElement.id}-error`);
+        inputElement.classList.add(this._object.errorclass);
+        errorElement.textContent = errorMessage;
+        errorElement.classList.add(this._object.errorMessageClass);
     }
-};
 
-
-
-
-//check if the whole form is valid
-const hasInvalidInput = (inputList) => {
-    return inputList.some((inputElement) => {
-        return !inputElement.validity.valid;
-    });
-};
-
-//inactive button
-const inactiveButton = (buttonElement) => {
-    buttonElement.disabled = true;
-    buttonElement.classList.add("button-inactive");
-};
-//active button
-const activeButton = (buttonElement) => {
-    buttonElement.disabled = false;
-    buttonElement.classList.remove("button-inactive");
-};
-
-//Activate the button and change the form background if the whole form is valid
-const toggleButtonState = (inputList, buttonElement) => {
-    if (hasInvalidInput(inputList)) {
-        inactiveButton(buttonElement);
+    _hideInputError(inputElement){
+        const errorElement= this._formSelector.querySelector(`#${inputElement.id}-error`);
+        inputElement.classList.remove(this._object.errorclass);
+        errorElement.textContent = "";
+        errorElement.classList.remove(this._object.errorMessageClass);
     }
-    else {
-        activeButton(buttonElement);
+    _checkInputValidity(inputElement){
+        if(!inputElement.validity.valid){
+            this._showInputError(inputElement,inputElement.validationMessage);
+        }
+        else{
+            this._hideInputError(inputElement);
+        }
     }
+
+    //check if the whole form is valid
+    _hasInvalidInput(){
+        return this._object.inputList.some((inputElement)=>{
+            return !inputElement.validity.valid;
+        })
+    }
+    
+    _inactiveButton(){
+        this._object.buttonElement.disabled = true;
+        this._object.buttonElement.classList.add(this._object.buttonClass);
+    }
+    
+    _activeButton(){
+        this._object.buttonElement.disabled = false;
+        this._object.buttonElement.classList.remove(this._object.buttonClass);
+    }
+
+   //Activate the button and change the form background if the whole form is valid
+   _toggleButtonState(){
+       if (this._hasInvalidInput()){
+           this._inactiveButton();
+       }
+       else{
+           this._activeButton();
+       }
+   }
+   _setEventListeners(){
+       //set button incative before inputing
+       this._toggleButtonState();
+       //trigger the input event for each input
+       this._object.inputList.forEach((inputElement)=>{
+           inputElement.addEventListener('input',()=>{
+               this._checkInputValidity(inputElement);
+               this._toggleButtonState();
+           })
+       })
+   }
+
+   enableValidatoin(){
+      this._formSelector.addEventListener("sumbit",(evt)=>{
+          evt.preventDefault();
+      });
+      this._setEventListeners();
+   }
+
+}
+
+
+const objectEdit={
+    errorClass:"formElement-error",
+    errorMessageClass:"formInput-errorMessage",
+    buttonClass:"button-inactive",
+    inputList:Array.from(formElement.querySelectorAll(`.edit__input`)),
+    buttonElement:formElement.querySelector('.edit__button')
+};
+
+const objectAdd={
+    errorClass:"formInput-error",
+    errorMessageClass:"formInput-errorMessage",
+    buttonClass:"button-inactive",
+    inputList:Array.from(addElements.querySelectorAll('.add__input')),
+    buttonElement:addElements.querySelector('.add__button')
 };
 
 
+const editValidClass= new FormValidator(objectEdit,formElement);
+const addValidClass= new FormValidator(objectAdd, addElements);
 
+editValidClass.enableValidatoin();
+addValidClass.enableValidatoin();
 
-//trigger the input event for edit form. formName are for two seperate forms 'edit' and 'add'
-const setEventListeners = (formSelector) => {
-    const formName = formSelector.className.split(" ")[0];
-    const inputList = Array.from(formSelector.querySelectorAll(`.${formName}__input`));
-    const buttonElement = formSelector.querySelector(`.${formName}__button`);
-    //set save button inactive before inputing
-    toggleButtonState(inputList, buttonElement);
-    //trigger the input event for each input
-    inputList.forEach((inputElement) => {
-        inputElement.addEventListener("input", () => {
-            checkInputValidity(formSelector, inputElement);
-            toggleButtonState(inputList, buttonElement);
-        });
-    });
-};
-
-
-
-
-const enableValidation = (formSelector) => {
-    //prevent default
-    formSelector.addEventListener("submit", (evt) => {
-        evt.preventDefault();
-    });
-    //call setEventListeners for edit and add form
-    setEventListeners(formSelector);
-};
-
-enableValidation(formElement);
-enableValidation(addElements);
 
